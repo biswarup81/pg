50f77efd0ba12fdf2ddebb00f47e4c96fcb549cf
Biswarup <biswarup.ghoshal@gmail.com>
2018-09-22 12:10:05 +0200

    Commit index.php

---
+++ about.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]><html lang="en" dir="ltr"  class="no-js ie8"><![endif]-->
+<!--[if IE 9 ]><html lang="en" dir="ltr"  class="no-js ie9"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html dir="ltr" class="no-js js wf-lato-n4-active wf-lato-n3-active wf-active" lang="en"><!--<![endif]-->
+<head>
+<script src="images_files/jquery_002_002.js" type="text/javascript"></script>
+<script type="text/javascript" src="images_files/jcarousellite_1.js"></script>
+<script type="text/javascript" src="images_files/tabs.js"></script>
+
+
+
+<script src="js/ga.js" type="text/javascript"></script>
+<script src="js/webfont.js" type="text/javascript"></script>
+
+
+
+<!--[if lte IE 7]> <div style=' text-align:center; clear: both; padding:0 0 0 15px; position: relative;'> <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a></div> <![endif]-->
+<title>PG Infoservices | About</title>
+<!--[if LT IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<link href="css/base.css" rel="stylesheet" type="text/css">
+<link href="css/boilerplate.css" rel="stylesheet" type="text/css">
+<link href="css/follow.css" rel="stylesheet" type="text/css">
+<link href="css/menus.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+
+</head>
+<body id="body" class="html front not-logged-in two-sidebars page-node with-navigation with-subnav">
+
+ <!--BEGIN wrapper-->   
+  <div id="page-wrapper">
+  
+ <!--BEGIN page--> 
+ 
+  <div id="page">
+ <!--BEGIN header--> 
+    <header id="header" role="banner" class="clearfix">
+        <div class="section-1 clearfix">
+         <!--BEGIN logo--> 
+        	<div class="col1">
+            	                    <a href="index.html" title="Home" rel="home" id="logo">
+                        <img src="images/logo.png" alt="Home"> </a>
+                            
+        </div>
+                            
+              <!--End of logo-->                
+                            
+                            
+        <!--BEGIN complain-->                      
+<div class="col2">
+                      <div class="region region-user-menu">
+    <section id="block-user-login" class="block block-user block-odd">
+
+     
+    
+  <div class="content">
+    <form action="/drupal_36661/?q=node&amp;destination=node" method="post" id="user-login-form" accept-charset="UTF-8"><div>
+ <h2>Post your complain </h2>
+<div class="form-actions form-wrapper" id="edit-actions"><input id="edit-submit" name="op" value="Click Here" class="form-submit" type="submit"></div></div></form>  
+</div>
+
+</section> </div>
+                </div>
+                
+        <!--Endcomplain--> 
+        
+        
+          <!--BEGIN menu--> 
+                          <div class="region region-menu">
+    <div id="block-superfish-1" class="block block-superfish block-even">
+
+      
+  <div class="content">
+    <ul id="superfish-1" class="sf-menu main-menu sf-horizontal sf-style-default sf-total-items-5 sf-parent-items-1 sf-single-items-4 sf-js-enabled">
+  
+    <li style="z-index: 14;" id="menu-1147-1" class="middle even sf-item-2 sf-depth-1 sf-total-children-2 sf-parent-children-1 sf-single-children-1 menuparent"><a href="index.php" title="" class="sf-depth-1"> Home</a></li>
+      <li style="z-index: 15;" id="menu-1170-1" class="middle odd sf-item-3 sf-depth-1 sf-no-children"><a href="sw/index.php" title="" class="sf-depth-1">Web and Software</a></li>
+      <li style="z-index: 14;" id="menu-1170-1" class="middle odd sf-item-4 sf-depth-1 sf-no-children"><a href="hw/index.php" title="" class="sf-depth-1">Hardware </a></li>
+<!--
+    <li style="z-index: 13;" id="menu-1170-1" class="middle odd sf-item-3 sf-depth-1 sf-no-children"><a href="careers.php" title="" class="sf-depth-1 ">Careers</a></li>
+-->    
+<li style="z-index: 12;" id="menu-668-1" class="middle even sf-item-4 sf-depth-1 sf-no-children"><a href="about.php" title="" class="sf-depth-1 active">About</a></li>
+    <li style="z-index: 11;" id="menu-442-1" class="last odd sf-item-5 sf-depth-1 sf-no-children"><a href="contacts.php" title="" class="sf-depth-1">Contacts</a></li></ul>  </div>
+
+</div></div>
+
+  <!--End of menu--> 
+        </div>
+
+		            <div class="section-2 clearfix">
+                  <div class="region region-header">
+    <section id="block-block-6" class="block block-block block-odd">
+
+        <h2 class="element-invisible">Slogan</h2>
+    
+  <div class="content">
+    <img alt="" src="images/slogan.png">  </div>
+
+</section><section id="block-views-services-block" class="block block-views block-even">
+
+        <h2 class="element-invisible">Services</h2>
+    
+      <div class="content">
+    <div class="view view-services view-id-services view-display-id-block view-dom-id-1">
+        
+  
+  
+      <div class="view-content">
+      
+      
+      
+      
+  <!--BEGIN men-conten--> 
+  <div id="content">
+  <!--BEGIN conten-left--> 
+   
+   <div class="conten-left">
+  <div class="left-bg2">
+  
+  <div class="text">
+  <h4><b>Our Philosophy</b></h4>
+  <ul style="clear:none;" class="feature-list mb0">
+                            <li style="padding-top:5px; padding-bottom:5px;">You Order, We Deliver.</li>
+                            <li style="padding-top:5px; padding-bottom:5px;">Low Cost, High Quality.</li>
+                          </ul>
+                          <br>
+
+  
+  </div>
+  </div>
+  
+  
+  <div class="left-bg">
+  
+  <div class="text">
+                          
+     <h4><b>Our Vision</b></h4>
+  <ul style="clear:none;" class="feature-list mb0">
+                            <li style="padding-top:5px; padding-bottom:5px;">Computer for every home</li>
+                            <li style="padding-top:5px; padding-bottom:5px;">Web enablement of every business houses</li>
+                          </ul>
+                          <br>
+   <h4><b>Our Mission</b></h4>
+  <ul style="clear:none;" class="feature-list mb0">
+                            <li style="padding-top:5px; padding-bottom:5px;">Outlets spread across rural & urban areas</li>
+                            <li style="padding-top:5px; padding-bottom:5px;">Deliver at Home*</li>
+                            <li style="padding-top:5px; padding-bottom:5px;">Onsite Service*</li>
+                            <li style="padding-top:5px; padding-bottom:5px;">Warranty* on hardware products</li>
+                          </ul>
+                          <br>
+
+  
+  </div>
+  </div>
+  
+  
+  
+  
+  
+   </div>
+    <!--End of conten-left--> 
+    
+    
+    
+         <!--BEGIN conten-right--> 
+     <div class="conten-right">
+     <h2>About the company</h2><br>
+  
+    <div class="text"><p><b>P.G.Infoservices</b> provides end-to-end computing solutions for both individual and business needs. Customers benefit from our expertise on computer hardware and softwares, mobile applications, rich web 2.0 internet application and websites at an affordable cost.</p>
+    
+    <h3><b>Our Services</b></h3>
+    <ul>
+                    <li style="padding-left:0px; background:none;"><b> &raquo; Technical Support Services (Hardware)</b></li>
+                 </ul>
+    
+   <ul style="padding-left:50px;">
+                        <li>Onsite Service</li>
+                        <li>Chip Level Repair</li>
+                        <li>AMC @ Low cost</li>
+                     </ul>
+                <br>
+                
+                <ul>
+                    <li style="padding-left:0px; background:none;"><b> &raquo; Softwares, Website Designs and Development</b></li>
+                 </ul>
+                 <p>We provides end-to-end web solutions for global businesses. We design, host and maintain websites. Across the globe, businesses benefit from our 											                 expertise in advanced and reliable technology soutions. Our domain expertise results in enhanced end user web experience.</p>
+                 
+                 <p>We offer a full range of solutions for personal, business, government and NGO customers including:</p>
+                 
+                 
+            <ul style="padding-left:50px;">
+                        <li>Hosting services</li>
+                        <li>Static &amp; Dynamic Web Pages</li>
+                        <li>Interactive Portal on web 2.0</li>
+                        <li>E-commerce solutions</li>
+                        <li>Payment Gateway</li>
+                     </ul>
+    
+    
+    
+    </div>
+
+  
+
+  
+
+   
+  </div>
+  
+ <!--End of conten-right-->   
+    
+    
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    </div>
+  
+  
+  
+  
+  
+  
+</div>   </div>
+
+</section> </div>
+            </div>
+        
+    </header>
+ 
+ <!--End of header--> 
+
+  
+  <!--BEGIN footer--> 
+    <footer id="footer" role="contentinfo">
+        <div class="footer-wrapper clearfix">
+		<div class="region region-footer">
+     <div id="block-follow-site" class="block block-follow block-odd">
+
+      
+  <div class="content">
+    <div class="follow-links clearfix">
+    <a href="#" class="follow-link follow-link-linkedin follow-link-site" title="Follow Centex Co. on LinkedIn">LinkedIn</a>
+  <a href="#" class="follow-link follow-link-facebook follow-link-site" title="Follow Centex Co. on Facebook">Facebook</a>
+  <a href="#" class="follow-link follow-link-twitter follow-link-site" title="Follow Centex Co. on Twitter">Twitter</a>
+  </div>  </div>
+
+</div><div id="block-block-5" class="block block-block block-even">
+
+      
+  <div class="content ftext">
+    &copy; 2012 <b>P.G.Infoservices</b> &nbsp;|&nbsp; <a href="#">Privacy Policy</a>  </div>
+
+</div><!-- /.block --><div id="block-block-8" class="block block-block block-odd">
+
+      
+  <div class="content">
+    <p><strong>E-mail</strong>: <a href="#">info@pginfoservices.com</a><br>
+Phone: <strong>+91.33.6540.2729</strong>
+  </div>
+
+</div> </div>
+                 </div>
+    </footer>
+ <!--End of footer--> 
+  </div>
+  <!-- End of page -->
+</div>
+<!-- End of wrapper -->
+
+
+
+</body></html>
---
+++ careers.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<!--[if IE 8 ]><html lang="en" dir="ltr"  class="no-js ie8"><![endif]-->
+<!--[if IE 9 ]><html lang="en" dir="ltr"  class="no-js ie9"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html dir="ltr" class="no-js js wf-lato-n4-active wf-lato-n3-active wf-active" lang="en"><!--<![endif]-->
+<head>
+<script src="images_files/jquery_002_002.js" type="text/javascript"></script>
+<script type="text/javascript" src="images_files/jcarousellite_1.js"></script>
+<script type="text/javascript" src="images_files/tabs.js"></script>
+
+
+
+<script src="js/ga.js" type="text/javascript"></script>
+<script src="js/webfont.js" type="text/javascript"></script>
+
+
+
+<!--[if lte IE 7]> <div style=' text-align:center; clear: both; padding:0 0 0 15px; position: relative;'> <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a></div> <![endif]-->
+<title>PG Infoservices | Careers</title>
+<!--[if LT IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<link href="css/base.css" rel="stylesheet" type="text/css">
+<link href="css/boilerplate.css" rel="stylesheet" type="text/css">
+<link href="css/follow.css" rel="stylesheet" type="text/css">
+<link href="css/menus.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+
+</head>
+<body id="body" class="html front not-logged-in two-sidebars page-node with-navigation with-subnav">
+
+ <!--BEGIN wrapper-->   
+  <div id="page-wrapper">
+  
+ <!--BEGIN page--> 
+ 
+  <div id="page">
+ <!--BEGIN header--> 
+    <header id="header" role="banner" class="clearfix">
+        <div class="section-1 clearfix">
+         <!--BEGIN logo--> 
+        	<div class="col1">
+            	                    <a href="index.html" title="Home" rel="home" id="logo">
+                        <img src="images/logo.png" alt="Home"> </a>
+                            
+            </div>
+                            
+              <!--End of logo-->                
+                            
+                            
+        <!--BEGIN complain-->                      
+<div class="col2">
+                      <div class="region region-user-menu">
+    <section id="block-user-login" class="block block-user block-odd">
+
+     
+    
+  <div class="content">
+    <form action="/drupal_36661/?q=node&amp;destination=node" method="post" id="user-login-form" accept-charset="UTF-8"><div>
+ <h2>Post your complain </h2>
+<div class="form-actions form-wrapper" id="edit-actions"><input id="edit-submit" name="op" value="Click Here" class="form-submit" type="submit"></div></div></form>  
+</div>
+
+</section> </div>
+                </div>
+                
+        <!--Endcomplain--> 
+        
+        
+          <!--BEGIN menu--> 
+                          <div class="region region-menu">
+    <div id="block-superfish-1" class="block block-superfish block-even">
+
+      
+  <div class="content">
+    <ul id="superfish-1" class="sf-menu main-menu sf-horizontal sf-style-default sf-total-items-5 sf-parent-items-1 sf-single-items-4 sf-js-enabled">
+  
+    <li style="z-index: 14;" id="menu-1147-1" class="middle even sf-item-2 sf-depth-1 sf-total-children-2 sf-parent-children-1 sf-single-children-1 menuparent"><a href="index.html" title="" class="sf-depth-1"> Home</a></li>
+      <li style="z-index: 15;" id="menu-1170-1" class="middle odd sf-item-3 sf-depth-1 sf-no-children"><a href="sw/index.php" title="" class="sf-depth-1">Web and Software</a></li>
+      <li style="z-index: 14;" id="menu-1170-1" class="middle odd sf-item-4 sf-depth-1 sf-no-children"><a href="hw/index.php" title="" class="sf-depth-1">Hardware </a></li>
+
+    <li style="z-index: 13;" id="menu-1170-1" class="middle odd sf-item-3 sf-depth-1 sf-no-children"><a href="careers.php" title="" class="sf-depth-1  active">Careers</a></li>
+    <li style="z-index: 12;" id="menu-668-1" class="middle even sf-item-4 sf-depth-1 sf-no-children"><a href="about.php" title="" class="sf-depth-1">About</a></li>
+    <li style="z-index: 11;" id="menu-442-1" class="last odd sf-item-5 sf-depth-1 sf-no-children"><a href="contacts.php" title="" class="sf-depth-1">Contacts</a></li></ul>  </div>
+
+</div></div>
+
+  <!--End of menu--> 
+        </div>
+
+		            <div class="section-2 clearfix">
+                  <div class="region region-header">
+    <section id="block-block-6" class="block block-block block-odd">
+
+        <h2 class="element-invisible">Slogan</h2>
+    
+  <div class="content">
+    <img alt="" src="images/slogan.png">  </div>
+
+</section><section id="block-views-services-block" class="block block-views block-even">
+
+        <h2 class="element-invisible">Services</h2>
+    
+      <div class="content">
+    <div class="view view-services view-id-services view-display-id-block view-dom-id-1">
+        
+  
+  
+      <div class="view-content">
+      
+      
+      
+      
+  <!--BEGIN men-conten--> 
+  <div id="content">
+  <!--BEGIN conten-left--> 
+   
+   <div class="conten-left">
+  <div class="left-bg2">
+  
+  <div class="text">
+  <div class="icon-to-left"><img width="24" height="24" alt="" src="images/key.png"></div>
+  <h4 style="font-size:14px;"><b>Looking for a change of pace?</b></h4>
+  <p>Want to join an exciting fun-to-work-in environment with a group of competing individuals striving to change the face of technology locally and globally? PG infoservices may be for you.</p>
+  
+  </div>
+  </div>
+  
+  
+  
+  
+  
+  
+  <div class="left-bg">
+  
+  <div class="text">
+  <div class="icon-to-left"><img width="24" height="24" alt="" src="images/network.png"></div>
+  <h4 style="font-size:14px;"><b>Looking for a change of pace?</b></h4>
+  <p>We're always looking for talented, potent individuals who are eager to do more with what they know and learn more to get ahead. <b>PG Infoservices</b> currently has open positions in the following areas:</p>
+  <ul>
+                        <li>PHP / mySQL Programming</li>
+                        <li>ASP.net / MSSQL Server Programming</li>
+                        <li>UI / UX Designing</li>
+                        <li>Systems Administration and Server Management</li>
+                        <li>Sale Service Professionals &amp; Web Marketing Executives</li>
+                    </ul>
+                    
+           <p style="padding-top:20px;">Please send your resume to <a href="mailto:pginfoservices@yahoo.com">pginfoservices@yahoo.com</a> mentioning the positions that you are interested in.</p>
+  
+  </div>
+  </div>
+  
+   </div>
+    <!--End of conten-left--> 
+    
+    
+    
+         <!--BEGIN conten-right--> 
+     <div class="conten-right">
+     <h2>Let's Work Together!</h2><br>
+     
+     <div class="text-1">At P.G. Infoservices, we've designed a culture that helps you get there. From our flexible, project-based approach to corporate structure to our innovative perks and benefits, we do everything we can to make sure our employees not only have great jobs, but great lives.</div><br>
+    <div class="text"><p>At P.G. Infoservices, we know that every employee has something important to say, and that every employee is integral to our success.</p>
+    
+    <p>Currently we are looking for :</p><br>
+    
+    <ul>
+                    <li><b>UI / UX Designers</b> (HTML5, CSS, Javascript, JQuery, Flash, Action Script, Photoshop)</li>
+                    <li><b>Web Marketing Executives</b></li>
+                    <li><b>PHP Developer</b> (PHP, MySQL, JQuery, Javascript, HTML5, CSS, OS Commerce, Drupal)</li>
+                    <li><b>Java Developer</b> (Core Java / OOPS, Java 5.0, Spring 2.5, MS SQL Server, HTML5, XML)</li>
+                    <li><b>.Net Developer</b> (ASP.NET 3.5, C#, SQL Server 2005/2008, JavaScript, JQuery, WCF,WPF)</li>
+                </ul>
+                <br>
+            <p style="padding-top:20px;">Send your resume to <a href="mailto:pginfoservices@yahoo.com">pginfoservices@yahoo.com</a></p>
+    
+    
+    
+    </div>
+
+  
+
+  
+
+   
+  </div>
+  
+ <!--End of conten-right-->   
+    
+    
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    </div>
+  
+  
+  
+  
+  
+  
+</div>   </div>
+
+</section> </div>
+            </div>
+        
+    </header>
+ 
+ <!--End of header--> 
+
+  
+  <!--BEGIN footer--> 
+    <footer id="footer" role="contentinfo">
+        <div class="footer-wrapper clearfix">
+		<div class="region region-footer">
+     <div id="block-follow-site" class="block block-follow block-odd">
+
+      
+  <div class="content">
+    <div class="follow-links clearfix">
+    <a href="#" class="follow-link follow-link-linkedin follow-link-site" title="Follow Centex Co. on LinkedIn">LinkedIn</a>
+  <a href="#" class="follow-link follow-link-facebook follow-link-site" title="Follow Centex Co. on Facebook">Facebook</a>
+  <a href="#" class="follow-link follow-link-twitter follow-link-site" title="Follow Centex Co. on Twitter">Twitter</a>
+  </div>  </div>
+
+</div><div id="block-block-5" class="block block-block block-even">
+
+      
+  <div class="content ftext">
+    &copy; 2012 <b>P.G.Infoservices</b> &nbsp;|&nbsp; <a href="#">Privacy Policy</a>  </div>
+
+</div><!-- /.block --><div id="block-block-8" class="block block-block block-odd">
+
+      
+  <div class="content">
+    <p><strong>E-mail</strong>: <a href="#">info@pginfoservices.com</a><br>
+Phone: <strong>+91.33.6540.2729</strong>
+  </div>
+
+</div> </div>
+                 </div>
+    </footer>
+ <!--End of footer--> 
+  </div>
+  <!-- End of page -->
+</div>
+<!-- End of wrapper -->
+
+
+
+</body></html>
